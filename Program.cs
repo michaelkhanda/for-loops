cc2b56846cd2c1bd0859b978495b9f9b84c54db4
Michael Khanda <michaelkhanda006@gmail.com>
2023-06-27 19:19:56 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace forloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score, total = 0;
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine("Enter a score: ");
+                score = int.Parse(Console.ReadLine());
+                total = total + score;
+            }
+            double average = total / 5;
+            Console.WriteLine("The average of the scores is:" + average);
+
+        }
+    }
+}
